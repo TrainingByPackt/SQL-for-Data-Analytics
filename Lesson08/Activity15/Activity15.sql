684d9824e607b94b140b922022dcbb2d0f6b50c9
reddangerous <dkinyanjui144@gmail.com>
2023-04-01 12:19:31 +0300

    Updated Activity15.sql creation of Function and calling the function to determine the average

--- Lesson08/Activity15/Activity15.sql
+++ Lesson08/Activity15/Activity15.sql
@@ -14,9 +14,9 @@ sqlda$# SELECT AVG(sales_amount) FROM sales INTO sales_avg WHERE sales_transacti
 -- Return the function variable, end the function and specify the language
 
 sqlda$# RETURN sales_avg;
-sqlda$# END; $channel_avg$
+sqlda$# END; $sales_avg$
 sqlda-# LANGUAGE PLPGSQL;
 
 -- Use the function to determine the average sales value since 2013-04-12
 
-sqlda=# SELECT avg_sales_since('2013-04-12', '2014-04-12');
+sqlda=# SELECT avg_sales_window('2013-04-12', '2014-04-12');
